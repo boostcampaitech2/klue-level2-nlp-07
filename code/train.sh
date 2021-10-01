edb1d70c3068777acbe594c5d294c854367983ef
Doohae <rick7213@gmail.com>
2021-10-01 15:46:17 +0000

    explanation for .sh file added

--- code/train.sh
+++ code/train.sh
@@ -3,3 +3,16 @@ python train.py \
     --epochs 1 \
     --save_dir my_dir \
     --dev_set False
+
+# 실행은 터미널 창에서 sh train.sh 입력
+# loop 돌려서 아래와 같이 여러 실험 원큐에도 가능
+# ex)
+# for BSZ in 16 32 64 ;
+# do
+#     for EPOCHS in 5 10 ;
+#     do
+#         python train.py \
+#             --bsz ${BSZ} \
+#             --epochs ${EPOCHS}
+#     done
+# done
