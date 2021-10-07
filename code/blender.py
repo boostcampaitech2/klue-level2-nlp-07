b2a25346e9a16a39cb7a96e4a74f83e674ff05a0
Doohae <rick7213@gmail.com>
2021-10-07 11:11:31 +0000

    additional files for submission

---
+++ code/blender.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import ast
+import numpy as np
+import pickle
+def num_to_label(label):
+  """
+    숫자로 되어 있던 class를 원본 문자열 라벨로 변환 합니다.
+  """
+  origin_label = []
+  with open('dict_num_to_label.pkl', 'rb') as f:
+    dict_num_to_label = pickle.load(f)
+  for v in label:
+    origin_label.append(dict_num_to_label[v])
+  
+  return origin_label
+output1_data = pd.read_csv('./prediction/submission_final2.csv')
+output2_data = pd.read_csv('./prediction/final.csv')
+
+output1_probs = []
+for string in output1_data['probs']:
+    probs = ast.literal_eval(string)
+    output1_probs.append(probs)
+
+output2_probs = []
+for string in output2_data['probs']:
+    probs = ast.literal_eval(string)
+    output2_probs.append(probs)
+
+weight1 = 0.7
+weight2 = 0.3
+output_probs = []
+single_probs = []
+for probs1, probs2 in zip(output1_probs, output2_probs):
+    for prob1, prob2 in zip(probs1, probs2):
+        single_probs.append(prob1 * weight1 + prob2 * weight2)
+    output_probs.append(single_probs)
+    single_probs = []
+
+output_preds = np.argmax(output_probs, axis=1).tolist()
+output_preds = num_to_label(output_preds)
+for output_prob in output_probs:
+    output_prob = str(output_prob)
+
+output = pd.DataFrame({'id':output1_data['id'],'pred_label':output_preds,'probs':output_probs,})
+output.to_csv('./prediction/final_real.csv', index=False)
--- code/create_augments.py
+++ code/create_augments.py
@@ -10,14 +10,15 @@ from tqdm import tqdm
 from random import random
 import random
 
-data = load_data("../dataset/train/train85.csv")
+data = load_data("../dataset/train/train.csv")
 
 
 ##################### 아래 alpha, prob, ratio, punctuations 등을 필요에 맞게 수정하세요 #####################
-eda = EDA(morpheme_analyzer="Mecab", alpha_sr=0.5, alpha_ri=0.1, alpha_rs=0, prob_rd=0)
-aeda = AEDA(morpheme_analyzer="Mecab", punc_ratio=0.05, punctuations=["?", "!", ".", ",", "##", "한편", "또한", "결국", "그래서", "따라서"])
+eda1 = EDA(morpheme_analyzer="Mecab", alpha_sr=0.3, alpha_ri=0.2, alpha_rs=0.1, prob_rd=0.1)
+eda2 = EDA(morpheme_analyzer="Okt", alpha_sr=0.3, alpha_ri=0.2, alpha_rs=0.1, prob_rd=0.1)
+aeda = AEDA(morpheme_analyzer="Mecab", punc_ratio=0.05, punctuations=["?", "!", ".", ",", "##", "한편", "또한", "결국", "그래서", "따라서", "사실", "그럼에도", "당연하게도", "그리고", "이어서"])
 #####################################################################################################
-FILE_NAME = 'train85_3.csv'
+FILE_NAME = 'train_okt.csv'
 ############################# 위에서 저장할 파일명을 .csv 형식으로 설정해주세요 ################################
 
 
@@ -48,35 +49,35 @@ def apply_deletion(text, e1, e2):
     text = re.sub('[·,]', ' , ', text)
     for word in lst:
         p = random.uniform(0, 1)
-        if p > 0.2 or never_delete(e1, e2, word):
+        if p > 0.15 or never_delete(e1, e2, word):
             new_words.append(word)
     aug_text = ' '.join(new_words)
     return aug_text
         
 
 #### Below is where all the augmentations created ###
-def augment_sent(df, my_eda, my_aeda):
+def augment_sent(df, my_eda1, my_eda2, my_aeda):
     sentences = list(df['sentence'])
     entities1, entities2 = list(df['subject_entity']), list(df['object_entity'])
     original = list(map(lambda sent, ent1, ent2: (sent, ent1, ent2), sentences, entities1, entities2))
     aug_sentences = []
     for sent, ent1, ent2 in tqdm(original, desc="Count 10 Seconds ..."):
         n = random.uniform(0, 1)
-        if n <= 0.2:
-            aug_sent = apply_augment(my_eda, sent, ent1, ent2)
-        elif n <= 0.3:
+        if n <= 0.4:
+            aug_sent = apply_augment(my_eda1, sent, ent1, ent2)
+        elif n <= 0.9:
+            aug_sent = apply_augment(my_eda2, sent, ent1, ent2)
+        elif n <= 0.95:
             aug_sent = apply_augment(my_aeda, sent, ent1, ent2)
-        elif n <= 0.6:
-            aug_sent = apply_deletion(sent, ent1, ent2)
         else:
-            aug_sent = sent
+            aug_sent = apply_deletion(sent, ent1, ent2)
         aug_sentences.append(aug_sent)
     output = pd.DataFrame({'id':df['id'],'sentence':aug_sentences,'subject_entity':entities1,'object_entity':entities2,'label':df['label'],})
     return output
 
 
 def main():
-    aug_output = augment_sent(data, eda, aeda)
+    aug_output = augment_sent(data, eda1, eda2, aeda)
     aug_output.to_csv("../dataset/train/" + FILE_NAME, index=False)
     
 
